5ff45ba3017bfea71d3b870b9f2cd66a3c0832a9
Ben Schwarz <ben.schwarz@gmail.com>
2009-03-17 16:49:54 +1100

    Added *everything*

---
+++ app.rb
@@ -0,0 +1,40 @@
+require 'rubygems'
+require 'sinatra'
+require 'open-uri'
+require 'json'
+
+module Feed
+  class << self
+    def base
+      # Make your own or clone mine
+      # http://pipes.yahoo.com/pipes/pipe.info?_id=e24258e68f6016bb6137f737552a203e
+      "http://pipes.yahoo.com/pipes/pipe.run?_id=e24258e68f6016bb6137f737552a203e"
+    end
+    
+    def get(ingredient = '')
+      uri = "#{base}&_render=json" 
+      uri << "&ingredient=#{ingredient}" unless ingredient.empty?
+      JSON.parse(open(uri).read)["value"]["items"]
+    end    
+  end
+end
+
+get '/' do
+  @recipes = Feed.get
+  erb :index
+end
+
+get '/ingredient/*' do
+  @recipes = Feed.get params[:splat].first
+  erb :index
+end
+
+post '/ingredient' do
+  @recipes = Feed.get params[:ingredient]
+  erb :index
+end
+
+get '/rss' do
+  redirect "#{Feed.base}&_render=rss"
+end
+
---
+++ config.ru
@@ -0,0 +1,5 @@
+require 'app'
+
+set :environment, :production
+
+run Sinatra::Application
